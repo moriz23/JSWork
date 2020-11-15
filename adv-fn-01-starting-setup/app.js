ec57d4491f8a090656e283cd34ebd4c7f55ad236
Mohammed Rizvi <morizvi07@gmail.com>
2020-11-14 22:28:41 -0500

    created folder

--- adv-fn-01-starting-setup/app.js
+++ adv-fn-01-starting-setup/app.js
@@ -80,3 +80,23 @@ function getFriendNames(person){
 }
 
 console.log(getFriendNames(myself));
+
+function random(min, max) {
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
+console.log(random(1, 10));
+
+function productDescription(strings, productName, productPrice){
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  return 'This is a product';
+}
+
+prodName = 'JavaScript Course'
+prodPrice = 29.99
+
+const productOutput = productDescription`This product ${prodName} is ${prodPrice}.`;
+
+console.log(productOutput);
+
